9940f250ac03608713aab91a3790523c61395451
Radomir Ralev <rado_ralev@yahoo.com>
2019-10-09 15:07:42 +0300

    "Adds functionality to add a different name when creating person if a person with the same name already exists."

--- Solution/src/commands/actions/CommandsConstants.java
+++ Solution/src/commands/actions/CommandsConstants.java
@@ -4,7 +4,8 @@ public class CommandsConstants {
     public static final String INVALID_COMMAND = "Invalid command name: %s!";
     public static final String PERSON_CREATED_MSG = "Person %s was created!";
     public static final String PERSON_DELETED_MSG = "Person %s was deleted!";
-    public static final String PERSON_EXISTS_ERROR_MSG = "Person %s already exists!";
+    public static final String PERSON_EXISTS_ERROR_MSG = "Name %s already exists in the database " +
+            "but you can add a family name or some identifier to create this person. Please change name: \n";
     public static final String INVALID_NUMBER_OF_ARGUMENTS = "Invalid number of arguments!";
     public static final String PERSONSLIST_IS_EMPTY = "Persons list is empty!";
     public static final String PERSONSLIST_INCLUDES = "Persons list includes: \n%s";
